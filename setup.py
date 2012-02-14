06d750f3a44751265d6a8359faa8ac5fe384640b
Harris Lapiroff <harris@chromamine.com>
2012-02-14 16:24:51 -0500

    Added a setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+version = __import__('daguerre').VERSION
+
+setup(
+		name='daguerre',
+		version='.'.join([str(v) for v in version]),
+		description='Image management and processing for Djanog.',
+		packages = find_packages(),
+	)
