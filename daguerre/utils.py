6a0814bcbdde585a74a241ca599c67d7141fb954
Harris Lapiroff <harris@chromamine.com>
2012-02-25 12:31:16 -0500

    Indentation fix in utils.py.

--- daguerre/utils.py
+++ daguerre/utils.py
@@ -199,20 +199,20 @@ def optimal_crop_dims(im, width, height, areas):
 
 
 def convert_filetype(ftype):
-"""Takes a file ending or mimetype and returns a valid mimetype or raises a ValueError."""
-if '.' in ftype:
-	try:
-		return mimetypes.types_map[ftype]
-	except KeyError:
-		return mimetypes.common_types[ftype]
-elif '/' in ftype:
-	if ftype in mimetypes.types_map.values() or ftype in mimetypes.common_types.values():
-		return ftype
+	"""Takes a file ending or mimetype and returns a valid mimetype or raises a ValueError."""
+	if '.' in ftype:
+		try:
+			return mimetypes.types_map[ftype]
+		except KeyError:
+			return mimetypes.common_types[ftype]
+	elif '/' in ftype:
+		if ftype in mimetypes.types_map.values() or ftype in mimetypes.common_types.values():
+			return ftype
+		else:
+			raise ValueError(_(u'Unknown MIME-type: %s' % ftype))
 	else:
-		raise ValueError(_(u'Unknown MIME-type: %s' % ftype))
-else:
-	raise ValueError(_('Invalid MIME-type: %s' % ftype))
-	
+		raise ValueError(_('Invalid MIME-type: %s' % ftype))
+
 
 ### Seam carving functions
 """
