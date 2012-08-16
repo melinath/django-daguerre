1b655b76b5f3f4f66ccd36747635b2b21b2bf4b3
Harris Lapiroff <harris@chromamine.com>
2012-08-16 15:06:41 -0400

    Prepped setup.py for mass distribution.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
 
 version = __import__('daguerre').VERSION
@@ -7,10 +8,28 @@ version = __import__('daguerre').VERSION
 setup(
 		name='daguerre',
 		version='.'.join([str(v) for v in version]),
-		description='Image management and processing for Djanog.',
-		packages = find_packages(),
-		install_requires = [
+		url="http://django-daguerre.readthedocs.org/",
+		description='Image management and processing for Django.',
+		long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+		license='BSD',
+		packages=find_packages(),
+		include_package_data=True,
+		zip_safe=False,
+		install_requires=[
 			'PIL',
 		],
-		include_package_data = True
+		extras_require={
+			'docs': ["sphinx>=1.0"],
+		},
+		classifiers=[
+			'Development Status :: 4 - Beta',
+			'Environment :: Web Environment',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: BSD License',
+			'Operating System :: OS Independent',
+			'Programming Language :: Python',
+			'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+			'Topic :: Multimedia :: Graphics',
+			'Framework :: Django',
+		],
 	)
