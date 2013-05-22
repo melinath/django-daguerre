8162e224b0702d93403bcc170471ff3f41b8c768
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-05-22 11:06:12 -0700

    Switched cost data to python lists.

--- daguerre/seam_carve.py
+++ daguerre/seam_carve.py
@@ -35,18 +35,18 @@ def get_cost(image):
     energy = generic_gradient_magnitude(im_arr, derivative=sobel)
     # Clip the energy to color values.
     energy = energy.clip(0, 255)
-    cost = numpy.zeros(energy.shape)
+    cost = []
     # initialize the bottom row.
-    cost[0] = energy[0].copy()
+    cost.append(list(energy[0]))
 
     for y in range(1, height):
-        for x in range(0, width):
-            # Find the lowest-cost x value in the three pixels below this.
-            bestcost = min([cost[y - 1, dx]
-                            for dx in (x - 1, x, x + 1)
-                            if dx >= 0 and dx < width])
-            # Add that cost to the energy of the current position.
-            cost[y, x] = bestcost + energy[y, x]
+        # Perhaps use row, last_row to avoid row lookup?
+        # row = [min([last_row[dx]...)]
+        last_row = cost[y - 1]
+        # iff x-1 wraps around, the first array will be empty.
+        cost.append([min(last_row[x - 1:x + 1] or
+                         last_row[x:x + 1]) + energy[y, x]
+                     for x in xrange(width)])
     return cost
 
 
@@ -57,30 +57,32 @@ def recalculate_cost(image, cost, seam):
                            (height, width))
     energy = generic_gradient_magnitude(im_arr, derivative=sobel)
     energy = energy.clip(0, 255)
-    new_cost = delete_seam(cost, seam)
-    new_cost = numpy.reshape(new_cost, (height, width))
+    for y, x in enumerate(seam):
+        del cost[y][x]
+
+    # Localize a few things to avoid lookups
     first_x = seam[0]
 
-    for y in range(0, height):
+    for y in range(1, height):
         # We start with one pixel left/right of the first deleted
         # pixel and go up in a cone.
         left = max((first_x - y - 1, 0))
         right = min((first_x + y, width))
-        for x in range(left, right):
-            bestcost = min([new_cost[y - 1, dx]
-                            for dx in (x - 1, x, x + 1)
-                            if dx >= 0 and dx < width])
-            new_cost[y, x] = bestcost + energy[y, x]
-    return new_cost
+        last_row = cost[y - 1]
+        cost[y][left:right] = [min(last_row[x - 1:x + 1] or
+                                   last_row[x:x + 1]) + energy[y, x]
+                               for x in xrange(left, right)]
+    return cost
 
 
 def get_seam(cost):
-    height, width = cost.shape
+    height = len(cost)
+    width = len(cost[0])
     mincost = numpy.inf
     x = 0
-    for dx in range(0, width):
-        if cost[-1, dx] < mincost:
-            mincost = cost[-1, dx]
+    for dx, val in enumerate(cost[-1]):
+        if val < mincost:
+            mincost = val
             x = dx
     seam = numpy.zeros(height, int)
     seam[height - 1] = x
@@ -89,8 +91,9 @@ def get_seam(cost):
         bestcost = numpy.inf
         for dx in (x - 1, x, x + 1):
             if dx >= 0 and dx < width:
-                if cost[y, dx] < bestcost:
-                    bestcost = cost[y, dx]
+                new_cost = cost[y][dx]
+                if new_cost < bestcost:
+                    bestcost = new_cost
                     x = dx
         seam[y] = x
 
