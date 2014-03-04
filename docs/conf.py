09de99c8041b910dda370f12b73135d91cf5d7d1
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-03-03 18:15:58 -0800

    Updated copyright notice in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Django Daguerre'
-copyright = u'2012, Stephen Burrows & Harris Lapiroff'
+copyright = u'2010-2014, Little Weaver Web Collective, LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
