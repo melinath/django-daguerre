b5e94eb055516094cf31b59f0b369a8fe8d0d01a
Filip Todic <filip.todic@styria.hr>
2016-10-14 14:24:55 +0200

    Detect warnings in upload_to tests
    The tests have been modified according to the official docs:
    * https://docs.python.org/2/library/warnings.html#testing-warnings

--- daguerre/tests/unit/test_models.py
+++ daguerre/tests/unit/test_models.py
@@ -1,3 +1,5 @@
+import warnings
+
 from daguerre.models import AdjustedImage, upload_to
 from daguerre.tests.base import BaseTestCase
 
@@ -61,32 +63,54 @@ class AdjustesImageUploadToTestCase(BaseTestCase):
         self.filename = '7014c0bdbedea0e4f4bf.jpeg'
 
     def test_upload_to__default_upload_dir(self):
-        hash_path = upload_to(instance=self.instance, filename=self.filename)
-        self.assertTrue(hash_path.startswith('dg/'))
-        self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
-        self.assertEqual(len(hash_path.split('/')), 4)
-        self.assertTrue(len(hash_path) < 45)
+        with warnings.catch_warnings(record=True) as w:
+            hash_path = upload_to(
+                instance=self.instance, filename=self.filename)
+            self.assertTrue(hash_path.startswith('dg/'))
+            self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
+            self.assertEqual(len(hash_path.split('/')), 4)
+            self.assertTrue(len(hash_path) < 45)
+            self.assertEqual(w, [])
 
     @override_settings(DAGUERRE_ADJUSTED_IMAGE_PATH='img')
     def test_upload_to__custom_upload_dir(self):
-        hash_path = upload_to(instance=self.instance, filename=self.filename)
-        self.assertTrue(hash_path.startswith('img/'))
-        self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
-        self.assertEqual(len(hash_path.split('/')), 4)
-        self.assertTrue(len(hash_path) < 45)
+        with warnings.catch_warnings(record=True) as w:
+            hash_path = upload_to(
+                instance=self.instance, filename=self.filename)
+            self.assertTrue(hash_path.startswith('img/'))
+            self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
+            self.assertEqual(len(hash_path.split('/')), 4)
+            self.assertTrue(len(hash_path) < 45)
+            self.assertEqual(w, [])
 
     @override_settings(DAGUERRE_ADJUSTED_IMAGE_PATH='0123456789123')
     def test_upload_to__custom_upload_dir_small(self):
-        hash_path = upload_to(instance=self.instance, filename=self.filename)
-        self.assertTrue(hash_path.startswith('0123456789123/'))
-        self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
-        self.assertEqual(len(hash_path.split('/')), 4)
-        self.assertTrue(len(hash_path) == 45)
+        with warnings.catch_warnings(record=True) as w:
+            hash_path = upload_to(
+                instance=self.instance, filename=self.filename)
+            self.assertTrue(hash_path.startswith('0123456789123/'))
+            self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
+            self.assertEqual(len(hash_path.split('/')), 4)
+            self.assertTrue(len(hash_path) == 45)
+            self.assertEqual(w, [])
 
     @override_settings(DAGUERRE_ADJUSTED_IMAGE_PATH='01234567891234')
     def test_upload_to__custom_upload_dir_big(self):
-        hash_path = upload_to(instance=self.instance, filename=self.filename)
-        self.assertTrue(hash_path.startswith('dg/'))
-        self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
-        self.assertEqual(len(hash_path.split('/')), 4)
-        self.assertTrue(len(hash_path) < 45)
+        with warnings.catch_warnings(record=True) as w:
+            hash_path = upload_to(
+                instance=self.instance, filename=self.filename)
+            self.assertTrue(hash_path.startswith('dg/'))
+            self.assertTrue(hash_path.endswith('/{}'.format(self.filename)))
+            self.assertEqual(len(hash_path.split('/')), 4)
+            self.assertTrue(len(hash_path) < 45)
+
+            # Test the warning message
+            # https://docs.python.org/2/library/warnings.html#testing-warnings
+            warning_message = ('The DAGUERRE_PATH value is more than 13 '
+                               'characters long! Falling back to the default '
+                               'value: "dg".')
+
+            self.assertEqual(len(w), 1)
+            user_warning = w[0]
+            self.assertEqual(user_warning.category, UserWarning)
+            self.assertEqual(user_warning.message.__str__(), warning_message)
